47912ab8eff007e73b64b69f74cd3342e946ba40
SarmaDinesh <dineshsarma551@gmail.com>
2026-09-15 15:44:17 -0500

    initial commit

---
+++ src/main/java/com/example/CompetencyHub/CompetencyHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.CompetencyHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetencyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetencyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CompetencyHub/CompetencyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CompetencyHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetencyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
